a5a374db48c9a5d077614dd07ef39ea722435e62
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-10-13 17:39:14 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ricky Primayuda Putra on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
